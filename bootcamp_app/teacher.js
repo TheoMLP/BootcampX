37e62e950c06c21d7a89434f67779466cc99a94b
TheoMLP <theomalaper.cognez@gmail.com>
2019-09-19 15:18:54 +0000

    implemented parametized query

--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -11,15 +11,20 @@ const params = {
 const pool = new Pool(params)
 pool.connect 
 
-pool.query(`
+const query = `
 SELECT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests 
 JOIN students ON students.id = student_id 
 JOIN cohorts ON cohorts.id = cohort_id 
 JOIN teachers ON teachers.id = teacher_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 GROUP BY teacher, cohort
-ORDER BY teacher;`)
+ORDER BY teacher;`
+
+const cohortName = process.argv[2]
+const values = [`%${cohortName}%`]
+
+pool.query(query, values)
     .then(res => {
       res.rows.forEach(row => {
         console.log(`${row.cohort}: ${row.teacher}`);
